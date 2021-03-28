085c8b3f2915a406fd1305f452ec5deba1738af3
Eric Ni <en1116@nyu.edu>
2021-03-28 17:00:40 -0400

    Big Initial Commit
    StageA: Sorry, forgot that we were submitting via a github link, so my initial progress is rather congested.  This initial commit includes an asset store pack for the main idle character with implementations of the stat systems as well as their effect on HP. Currently, there are still tweaks that need to be made to the math and being able to undo or alter certain stats.

---
+++ Assets/Scenes/HPSPManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HPSPManager : MonoBehaviour
+{
+    public TMP_InputField strengthInput;
+    public TMP_InputField toughnessInput;
+    public TMP_InputField dexterityInput;
+    public TMP_InputField intellectInput;
+    public TMP_InputField powerInput;
+    public TMP_InputField charmInput;
+
+    public TextMeshProUGUI HP;
+    public TextMeshProUGUI SP;
+
+    public int Health;
+    public int Spells;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = int.Parse(HP.text);
+        Spells = int.Parse(SP.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if (int.Parse(strengthInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            {
+                
+                Health = (15 * int.Parse(strengthInput.text));
+                Spells = (10 * int.Parse(strengthInput.text));
+
+            } if (int.Parse(toughnessInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            
+                
+                Health = (25 * int.Parse(toughnessInput.text));
+                Spells = (0 * int.Parse(toughnessInput.text));
+
+            } if (int.Parse(dexterityInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            {
+                
+                Health = (10 * int.Parse(dexterityInput.text));
+                Spells = (15 * int.Parse(dexterityInput.text));
+
+            } if (int.Parse(intellectInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            {
+                
+                Health = (0 * int.Parse(intellectInput.text));
+                Spells = (25 * int.Parse(intellectInput.text));
+
+            }  if (int.Parse(powerInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            {
+                Health = (20 * int.Parse(powerInput.text));
+                Spells = (5 * int.Parse(powerInput.text));
+
+            }  if (int.Parse(charmInput.text) > 0 && int.Parse(strengthInput.text) <= 50)
+            {
+
+                Health = (5 * int.Parse(charmInput.text));
+                Spells = (20 * int.Parse(charmInput.text));
+
+            } if (int.Parse(strengthInput.text) == 0 && int.Parse(toughnessInput.text) == 0 && int.Parse(dexterityInput.text) == 0 
+            && int.Parse(intellectInput.text) == 0 && int.Parse(powerInput.text) == 0 && int.Parse(charmInput.text) == 0) {
+
+                Health = 1;
+                Spells = 1;
+            }
+
+
+
+            HP.text = Health.ToString();
+            SP.text = Spells.ToString();
+
+        }
+    
+}
---
+++ Assets/Scenes/StatsButtons.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatsButtons : MonoBehaviour
+{
+    public TextMeshProUGUI statPoints;
+
+    public TMP_InputField statInput;
+    public bool textIsChanged;
+    public int pointsSubtracted;
+    public float timer = 0;
+
+    public int temp;
+    public TextMeshProUGUI popup;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        temp = 0;
+        statInput = GetComponent<TMP_InputField>();
+        textIsChanged = false;
+        //statInput.characterValidation = TMP_InputField.CharacterValidation.Integer;
+    }
+
+    void Update()
+    {
+        if(popup.enabled == true)
+        {
+            timer += Time.deltaTime;
+            if(timer > 3)
+            {
+                timer = 0;
+                popup.enabled = false;
+            }
+        }
+    }
+
+    public void Text_Changed()
+    {
+
+        if (int.TryParse(statInput.text, out temp))
+        {
+            temp = int.Parse(statInput.text);
+        } else
+        {
+            temp = 0;
+        }
+        
+        //temp = int.Parse(str);
+        //Debug.Log(temp);
+
+        int tempPoints;
+            tempPoints = int.Parse(statPoints.text);
+
+        if (tempPoints - temp >= 0 && pointsSubtracted == 0)
+        {
+            statPoints.text = (tempPoints - temp).ToString();
+            pointsSubtracted = temp;
+        } else if (( tempPoints - temp >= 0) && pointsSubtracted > 0)
+        {
+
+            statPoints.text = (tempPoints - temp + pointsSubtracted).ToString();
+            pointsSubtracted = temp;
+        }
+        else if (tempPoints - temp < 0)
+        {
+            
+            popup.enabled = true;
+        }
+
+
+
+    }
+}
+
+
+
+
